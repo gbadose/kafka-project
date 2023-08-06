80943c04b38cc655c20c043ab1031c7f14e623e9
alain <glody.albadose@gmail.com>
2023-08-06 11:40:43 -0500

    created demo project

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,48 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("I am a Kafka producer.");
+
+
+        // create producer properties
+        Properties properties = new Properties();
+
+        // connect to localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        properties.setProperty("bootstrap.servers","cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"7eqhy6DnSYCV4IrTpDSdzF\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiI3ZXFoeTZEblNZQ1Y0SXJUcERTZHpGIiwib3JnYW5pemF0aW9uSWQiOjc1NDQ3LCJ1c2VySWQiOjg3Nzc3LCJmb3JFeHBpcmF0aW9uQ2hlY2siOiIxOWI0YzFkNC1lMGJlLTRjMWEtYjRkZS01N2RhMDc4NzJhYmYifX0.AuFk_vqpagtFzamYsNTgS_S2vLYvpPxok2cfzT0aUVA\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        //set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(producerRecord);
+
+
+        // flush and close producer
+        producer.flush();
+        producer.close();
+
+    }
+}
